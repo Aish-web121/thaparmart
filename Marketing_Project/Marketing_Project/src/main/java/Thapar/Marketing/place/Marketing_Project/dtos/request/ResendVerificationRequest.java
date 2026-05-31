504792c1889a2931a1d693ea0c8b5d2281d16bd9
Aishwarya Ranjan <ranjanaishwarya8@gmail.com>
2026-06-01 02:02:35 +0530

    Update ResendVerificationRequest.java

--- Marketing_Project/Marketing_Project/src/main/java/Thapar/Marketing/place/Marketing_Project/dtos/request/ResendVerificationRequest.java
+++ Marketing_Project/Marketing_Project/src/main/java/Thapar/Marketing/place/Marketing_Project/dtos/request/ResendVerificationRequest.java
@@ -4,10 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
-@Data
+@Data 
 public class ResendVerificationRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Must be a valid email")
     private String email;
 }
+//
