73aa5683405230c1f452a86340d1a8af0a5d253f
Aishwarya Ranjan <ranjanaishwarya8@gmail.com>
2026-06-01 18:07:49 +0530

    Update RegisterRequest.java

--- Marketing_Project/Marketing_Project/src/main/java/Thapar/Marketing/place/Marketing_Project/dtos/request/RegisterRequest.java
+++ Marketing_Project/Marketing_Project/src/main/java/Thapar/Marketing/place/Marketing_Project/dtos/request/RegisterRequest.java
@@ -1,29 +1,19 @@
 package Thapar.Marketing.place.Marketing_Project.dtos.request;
-
 import Thapar.Marketing.place.Marketing_Project.enums.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
 @Data
 public class RegisterRequest {
-
     @NotBlank(message = "Name is required")
     private String name;
-
     @NotBlank(message = "Email is required")
     @Email(message = "Must be a valid email")
     private String email;
-
     @NotBlank(message = "Password is required")
     private String password;
-
-    // only ROLE_BUYER or ROLE_SELLER allowed at registration
-    // ROLE_ADMIN is seeded manually
-    @NotNull(message = "Role is required")
-    private Role role;
-
+    // Role defaults to SELLER; ADMIN cannot be set via registration
+    private Role role = Role.SELLER;
     private String college;
     private String hostelName;
     private String hostelRoom;
